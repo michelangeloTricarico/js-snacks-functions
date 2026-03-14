3e6dbba87bea658a6f4f598a498b21a38dc2e8d6
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-03-14 08:08:27 +0100

    Numero vocali exercise

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,23 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+let vowels = (string) =>{
+    let num_vowels = 0
+    for (let i=0; i<string.length; i++){
+        if (string[i].toUpperCase() == "A" || 
+            string[i].toUpperCase() == "E" || 
+            string[i].toUpperCase() == "I" || 
+            string[i].toUpperCase() == "O" ||
+            string[i].toUpperCase() == "U"){
+                num_vowels++
+            }
+    }
+    return num_vowels
+} 
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(vowels(word))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
