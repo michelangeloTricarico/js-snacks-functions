baa9457d2cda4b57f9a05f70dbb4eb5347bb113c
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-03-14 08:15:05 +0100

    Bonus exercise

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,22 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+const sayHello = (name_input) =>{
+    let data = new Date();
+    if (data.getHours()>0 && data.getHours()<=13){
+        return ("Buongiorno "+name_input)
+    }
+    else if (data.getHours()>13 && data.getHours()<=17){
+        return ("Buon pomeriggio "+name_input)
+    }
+    else if (data.getHours()>17 && data.getHours()<=24){
+        return ("Buonasera "+name_input)
+    }
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(sayHello(name))
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
